303bd5e8292080aebf72961a205cf226cc8fd932
darkoyd <n.londonoc@uniandes.edu.co>
2025-03-20 12:37:11 -0500

    HOTFIX: Rolled back build.gradle

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,7 +66,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.compose.material:material:1.5.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
     // Amplify dependency
     implementation("com.amplifyframework:core-kotlin:2.24.0")
