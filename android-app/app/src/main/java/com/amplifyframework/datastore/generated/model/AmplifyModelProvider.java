0033abe8be857f8a41ccaf8b25f67f12b65ca036
darkoyd <n.londonoc@uniandes.edu.co>
2025-03-21 10:06:18 -0500

    Added missing models

--- android-app/app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
+++ android-app/app/src/main/java/com/amplifyframework/datastore/generated/model/AmplifyModelProvider.java
@@ -13,7 +13,7 @@ import java.util.Set;
  */
 
 public final class AmplifyModelProvider implements ModelProvider {
-  private static final String AMPLIFY_MODEL_VERSION = "b3a43290f810b17600369d3962863356";
+  private static final String AMPLIFY_MODEL_VERSION = "c8e35bd7f166477ff7479fad230463b6";
   private static AmplifyModelProvider amplifyGeneratedModelInstance;
   private AmplifyModelProvider() {
     
@@ -34,7 +34,7 @@ public final class AmplifyModelProvider implements ModelProvider {
   @Override
    public Set<Class<? extends Model>> models() {
     final Set<Class<? extends Model>> modifiableSet = new HashSet<>(
-          Arrays.<Class<? extends Model>>asList(Wishlist.class, BookWishlist.class, Book.class, UserLibrary.class, BookLibrary.class, BookCategory.class, Author.class, User.class, UserRating.class, Notifications.class, Category.class, BookRating.class, Listing.class, Cart.class)
+          Arrays.<Class<? extends Model>>asList(Wishlist.class, BookWishlist.class, Book.class, UserLibrary.class, BookLibrary.class, BookCategory.class, Author.class, User.class, UserRating.class, Category.class, BookRating.class, Listing.class, Notification.class, Cart.class)
         );
     
         return Immutable.of(modifiableSet);
---
+++ android-app/app/src/main/java/com/amplifyframework/datastore/generated/model/NotificationPath.java
@@ -0,0 +1,14 @@
+package com.amplifyframework.datastore.generated.model;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.amplifyframework.core.model.ModelPath;
+import com.amplifyframework.core.model.PropertyPath;
+
+/** This is an auto generated class representing the ModelPath for the Notification type in your schema. */
+public final class NotificationPath extends ModelPath<Notification> {
+  NotificationPath(@NonNull String name, @NonNull Boolean isCollection, @Nullable PropertyPath parent) {
+    super(name, isCollection, parent, Notification.class);
+  }
+}
